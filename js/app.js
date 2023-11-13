c9f23017f902de51aa2392ed64940f7980851cb6
Spirich <125336693+Spirichyels@users.noreply.github.com>
2023-11-13 12:26:55 +0500

    app.js

--- js/app.js
+++ js/app.js
@@ -7,24 +7,46 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 		smooth: 1.5,
 		effects: true,
 	
-	});
-
-
+	})
 
 	gsap.fromTo('.hero-section', {opacity: 1}, {
 		opacity: 0,
 		scrollTrigger: {
 			trigger: '.hero-section',
 			start: 'center',
-			end: '800',
+			end: 'bottom',
 			scrub: true
 		}
 	})
 
 
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
 
+	itemsL.forEach(item => {
+		gsap.fromTo(item, {x: -50, opacity: 0}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: 'true'
+			}
+		})
+	});
+
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, {x: 50, opacity: 0}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: 'true'
+			}
+		})
+	});
 }
-
-
-
